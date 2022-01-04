df7523eef93539a33d761897a46af712faa1dcfe
Junaid Rahim <junaidrahim8d@gmail.com>
2022-01-05 00:56:56 +0530

    added errors

--- cmd/fornaxdb/main.go
+++ cmd/fornaxdb/main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"github.com/FornaxDB/fornaxdb/logger"
+	"github.com/FornaxDB/fornaxdb/errors"
 )
 
 
 func main() {
 	l := logger.New()
 	l.Trace("Hello, World!", map[string]interface{}{"foo": "bar"})
+	err := x()
+	if err != nil {
+		l.Fatal(err.Error(), map[string]interface{}{})
+	}
+}
+
+func x() error {
+	return errors.SchemaAlreadyExists.New("bla")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/FornaxDB/fornaxdb
 
 go 1.17
+
+require (
+	github.com/joomcode/errorx v1.0.3 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
