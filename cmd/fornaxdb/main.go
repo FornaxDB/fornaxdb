1779a39f79daae4c3d96dbba0daadea581414bc3
Aditya Meharia <adityameharia14@gmail.com>
2022-01-05 14:18:34 +0000

    test

--- cmd/fornaxdb/main.go
+++ cmd/fornaxdb/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	l := logger.New()
-	l.Trace("Hello, World!", map[string]interface{}{"foo": "bar"})
+	l.Trace("Hello, World!", map[string]interface{}{"fo": "bar"})
 	err := x()
 	if err != nil {
 		l.Fatal(err.Error(), map[string]interface{}{})
