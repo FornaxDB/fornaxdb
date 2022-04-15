54aee471eade649f29b11f98538e2d27b8d76299
Junaid Rahim <junaidrahim8d@gmail.com>
2022-04-16 00:37:43 +0530

    fix(storage): fix documentation in nodestore

--- storage/nodestore/init.go
+++ storage/nodestore/init.go
@@ -7,11 +7,13 @@ import (
 
 var LOGGER = logger.New()
 
+// Init initialises a NodeStore
 func (n *NodeStore) Init(file *os.File, position int64) {
 	n.File = file
 	n.Position = position
 }
 
+// Close closes the file held by the NodeStore
 func (n *NodeStore) Close() error {
 	err := n.File.Close()
 	if err != nil {
