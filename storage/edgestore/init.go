614b74425f94126a1bdaed4fa119d9a1e2fe7d1f
Junaid Rahim <junaidrahim8d@gmail.com>
2022-04-16 00:21:04 +0530

    fix(storage): fix documentation in edgestore

--- storage/edgestore/init.go
+++ storage/edgestore/init.go
@@ -7,16 +7,20 @@ import (
 
 var LOGGER = logger.New()
 
+// Init initialises an EdgeStore
 func (n *EdgeStore) Init(file *os.File, position int64) {
 	n.File = file
 	n.Position = position
+	LOGGER.Info("Initialized EdgeStore", nil)
 }
 
+// Close closes the file held by the EdgeStore
 func (n *EdgeStore) Close() error {
 	err := n.File.Close()
 	if err != nil {
 		return err
 	}
 
+	LOGGER.Info("Closing EdgeStore", nil)
 	return nil
 }
